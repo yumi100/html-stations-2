e9a4fad9cb3e1088358ef22965ddeef2965f55c5
yumi100 <yyummik@gmail.com>
2022-11-13 22:14:46 +0900

    clear

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const myp = document.getElementById("text");
+  if (check.checked) {
+    myp.style = "background-color:red;";
+  } else {
+    myp.style = "backgroundcolor:transparent;";
+  }
 }
